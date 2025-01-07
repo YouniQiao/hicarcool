471df75a995702ae76fdb00f48f678887cc1e9fd
油腻樵夫 <129544859+YouniQiao@users.noreply.github.com>
2025-01-07 17:30:12 +0800

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -227,10 +227,16 @@ export default function HomepageFeatures() {
         <td>在正式版本之上额外开放花瓣地图以及微信投屏的版本。通过会员中心体验官下载。</td>
         </tr>
         <tr>
+        <td>2025.01</td>
+        <td><a href="https://hicar.lanzouq.com/ibzuK2k8nrmd" target="_blank"><font color="red">14.2.0.295</font></a></td>
+        <td></td>
+        <td></td>
+        </tr>
+        <tr>
         <td>2024.10～11</td>
         <td><a href="https://hicar.lanzouq.com/iLHOq2cv18ej" target="_blank">14.2.0.245</a></td>
-        <td><a href="https://hicar.lanzouq.com/isdn22edsbva" target="_blank"><font color="red">14.2.0.255</font></a></td>
-        <td></td>
+        <td><a href="https://hicar.lanzouq.com/isdn22edsbva" target="_blank">14.2.0.255</a></td>
+        <td> - </td>
         </tr>
         <tr>
         <td>2024.09</td>
